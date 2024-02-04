25592da8fbacc7ec65ef0892e24d17de7df16320
nagasekhar333 <111780117+nagasekhar333@users.noreply.github.com>
2024-02-04 14:12:56 +0530

    first commit

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "manduri" # Replace with your actual S3 bucket name
-    key    = "na"
+    key    = "Jenkins-terraform.tfstate"
     region = "ap-south-1"
   }
 }
