c1cfbea7a8675a12b3bd99ef2a4015a813f59f76
nagasekhar333 <111780117+nagasekhar333@users.noreply.github.com>
2024-02-04 15:03:21 +0530

    Update Main.tf

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -58,7 +58,7 @@ resource "aws_security_group" "Jenkins-sg" {
   }
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "main" {
   ami                    = "ami-0df4b2961410d4cff"
   instance_type          = "t2.medium"
   key_name               = "na"
